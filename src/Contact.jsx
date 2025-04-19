63956afadf4104abf11b5e59b33bc848216d9951
bryanmayodi <bryanmayodi@gmail.com>
2025-04-19 13:02:45 +0300

    changed all image links from absolute paths

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,14 +1,17 @@
+import mail from "./assets/mail-icon.png";
+import phone from "./assets/phone-icon.png";
+
 export default function Contact(props) {
   return (
     <article className="contact-card">
       <img src={props.img} alt={`Photo of ${props.name}`} />
       <h3>{props.name}</h3>
       <div className="info-group">
-        <img src="src/assets/phone-icon.png" alt="phone icon" />
+        <img src={phone} alt="phone icon" />
         <p>{props.phone}</p>
       </div>
       <div className="info-group">
-        <img src="src/assets/mail-icon.png" alt="mail icon" />
+        <img src={mail} alt="mail icon" />
         <p>{props.mail}</p>
       </div>
     </article>
